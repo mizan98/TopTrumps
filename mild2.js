7438b09824fd28a49dfa86c509bdb0b3ef9be7a5
Mizan Choudhury <mizan_98@hotmail.co.uk>
2019-11-09 09:26:39 +0000

    defence stats added

--- mild2.js
+++ mild2.js
@@ -1,34 +1,34 @@
 let card = [
-    {name: "pichu",  strength: 40},
-    {name: "pikachu", strength: 50},
-    {name : "raichu", strength: 60},
-    {name : "bulbasaur", strength: 40},
-    {name: "ivysaur", strength: 50},
-    {name: "venusaur", strength: 60},
-    {name: "charmander", strength: 40},
-    {name: "charmeleon", strength: 50},
-    {name: "charizard", strength: 60},
-    {name: "squirtle", strength: 40},
-    {name: "wartortle", strength: 50},
-    {name: "blastoise", strength: 60},
-    {name: "onyx", strength: 70},
-    {name: "steelix", strength: 80},
-    {name: "ponyta", strength: 60},
-    {name: "caterpee", strength: 20},
-    {name: "metapod", strength: 30},
-    {name: "taurus", strength: 60},
-    {name: "tentacool", strength: 50},
-    {name: "tentacruel", strength: 60},
-    {name: "koffing", strength: 30},
-    {name: "weezing", strength: 40},
-    {name: "snorlax", strength: 70},
-    {name: "pidgey", strength: 20},
-    {name: "pidgeotto", strength: 30},
-    {name: "pidget", strength: 40},
-    {name: "ekans", strength: 50},
-    {name: "arbok", strength: 60},
-    {name: "lickitung", strength: 50},
-    {name: "meowth", strength: 40}
+    {name: "pichu",  strength: 40, defence: 30},
+    {name: "pikachu", strength: 50, defenece: 40},
+    {name : "raichu", strength: 60, defence: 50},
+    {name : "bulbasaur", strength: 40, defence: 50},
+    {name: "ivysaur", strength: 50, defence: 70},
+    {name: "venusaur", strength: 60, defence: 80},
+    {name: "charmander", strength: 40, defence: 30},
+    {name: "charmeleon", strength: 50, defence: 40},
+    {name: "charizard", strength: 60, defence: 70},
+    {name: "squirtle", strength: 40, defence: 30},
+    {name: "wartortle", strength: 50, defence: 50},
+    {name: "blastoise", strength: 60, defence:80},
+    {name: "onyx", strength: 70, defence: 75},
+    {name: "steelix", strength: 80, defence: 90},
+    {name: "ponyta", strength: 60, defence: 60},
+    {name: "caterpee", strength: 20, defence: 20},
+    {name: "metapod", strength: 30, defence: 30},
+    {name: "taurus", strength: 60, defence: 50},
+    {name: "tentacool", strength: 50, defence: 40},
+    {name: "tentacruel", strength: 60, defence: 50},
+    {name: "koffing", strength: 30, defence: 30},
+    {name: "weezing", strength: 40, defence: 20},
+    {name: "snorlax", strength: 70, defence: 100},
+    {name: "pidgey", strength: 20, defence: 10},
+    {name: "pidgeotto", strength: 30, defence: 20},
+    {name: "pidget", strength: 40, defence: 30},
+    {name: "ekans", strength: 50, defence: 40},
+    {name: "arbok", strength: 60, defence: 50},
+    {name: "lickitung", strength: 50, defence: 50},
+    {name: "meowth", strength: 40, defence: 20}
 ]
 
 const shuffle = (a) => {
@@ -65,6 +65,7 @@ class NewGame {
             plyr1Deck.push(player1Hand, player2Hand)
             alert(`${this._players1} wins this round.`)
             alert(`${this._players1} now has ${plyr1Deck.length} cards. ${this._players1} starts the next round.`)
+            console.log(plyr1Deck)
             alert(`${this._players2} now has ${plyr2Deck.length} cards.`)
             this.checkVictory()
         }
@@ -72,6 +73,7 @@ class NewGame {
             plyr2Deck.push(player2Hand, player1Hand)
             alert(`${this._players2} wins this round.`)
             alert(`${this._players2} now has ${plyr2Deck.length} cards. ${this._players2} starts the next round.`)
+            console.log(plyr2Deck)
             alert(`${this._players1} now has ${plyr1Deck.length} cards.`)
             this.checkVictoryP2()
         }
